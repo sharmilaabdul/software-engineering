6080f6f692d7289766a3cd9d1e5c5e2db1c58579
sharmilaabdul <sabdul3@gitam.in>
2023-12-27 20:24:14 +0530

    Create code 5.py

---
+++ code 5.py
@@ -0,0 +1,17 @@
+import math
+
+def quadratic_solution(a, b, c):
+    delta = b**2 - 4*a*c  # Use '*' for multiplication, not '%'
+    if delta >= 0:
+        root1 = (-b + math.sqrt(delta)) / (2*a)
+        root2 = (-b - math.sqrt(delta)) / (2*a)
+        return root1, root2
+    else:
+        return "Complex roots - No real solution"
+
+coefficient_sets = [(2, -6, 1), (1, 4, -6), (-1, 6, -4)]
+
+for coefficients in coefficient_sets:
+    a_set, b_set, c_set = coefficients
+    roots_set = quadratic_solution(a_set, b_set, c_set)
+    print(f"Set input roots: {roots_set}")
